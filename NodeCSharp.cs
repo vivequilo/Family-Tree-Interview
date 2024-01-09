fe54edacc8f95d5d08eb9a6636d37e9308a10832
aeaa1998 <47725760+aeaa1998@users.noreply.github.com>
2024-01-09 15:22:57 -0600

    Update NodeCSharp.cs

--- NodeCSharp.cs
+++ NodeCSharp.cs
@@ -101,21 +101,21 @@ public class HelloWorld
 
         Node[] treeJson = new Node[15];
 
-        treeJson[1 - 1] = new Node(1,"Pedro A", null);
-        treeJson[2 - 1] = new Node(2,"Pedro H", 1);
-        treeJson[3 - 1] = new Node(3,"Lorena", 1);
-        treeJson[4 - 1] = new Node(4,"Luis", 2);
-        treeJson[5 - 1] = new Node(5,"Jimena", 3);
-        treeJson[6 - 1] = new Node(6,"Lucia", 2);
-        treeJson[7 - 1] = new Node(7,"Augusto", 6);
-        treeJson[8 - 1] = new Node(8,"Hsing Li", 6);
-        treeJson[9 - 1] = new Node(9,"Carmen", 8);
-        treeJson[10 - 1] = new Node(10,"Gabriel", 7);
-        treeJson[11 - 1] = new Node(11,"Juan", 7);
-        treeJson[12 - 1] = new Node(12,"Isabella", 5);
-        treeJson[13 - 1] = new Node(13,"Cristina", 5);
-        treeJson[14 - 1] = new Node(14,"Julia", 10);
-        treeJson[15 - 1] = new Node(15,"Javier", 11);
+        treeJson[0] = new Node(1,"Pedro A", null);
+        treeJson[1] = new Node(2,"Pedro H", 1);
+        treeJson[2] = new Node(3,"Lorena", 1);
+        treeJson[3] = new Node(4,"Luis", 2);
+        treeJson[4] = new Node(5,"Jimena", 3);
+        treeJson[5] = new Node(6,"Lucia", 2);
+        treeJson[6] = new Node(7,"Augusto", 6);
+        treeJson[7] = new Node(8,"Hsing Li", 6);
+        treeJson[8] = new Node(9,"Carmen", 8);
+        treeJson[9] = new Node(10,"Gabriel", 7);
+        treeJson[10] = new Node(11,"Juan", 7);
+        treeJson[11] = new Node(12,"Isabella", 5);
+        treeJson[12] = new Node(13,"Cristina", 5);
+        treeJson[13] = new Node(14,"Julia", 10);
+        treeJson[14] = new Node(15,"Javier", 11);
         
         Node root = getTree(treeJson);
         
