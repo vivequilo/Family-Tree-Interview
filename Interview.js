ad31336f9fb57f087996e4ee532723be56d7550d
aeaa1998 <47725760+aeaa1998@users.noreply.github.com>
2024-01-09 12:20:24 -0600

    Update Interview.js

--- Interview.js
+++ Interview.js
@@ -44,25 +44,31 @@ class Node
 }
 
 
-let getTree = (list) => {
+//Helper function to build a tree from a list of JSON items
+const getTree = (list) => {
+    //Create a object to hold the mapping of the nodes
     let nodes = {}
-    
+    //Create a variable to store later the root of the whole tree
     let root = undefined
+    //Iterate in the list of json elements
     list.forEach((json) => {
-        //Create the node from each one of the elements
+        Create a new instance of the Node class from the json
         let node = new Node(json)
-        //Map it into the json
+        //We will map it using the id
         nodes[`${json.id}`] = node
-        //If no parent means the root
+        //If there is no parentId key in the json item it means is the root node
         if(!json.parentId){
             root = node
         }
     })
     
+    //Iterate on each one of the keys of the nodes mapped
     Object.keys(nodes).map((id) => {
+        //Retrieve the node from the map corresponding to the current key
         let node = nodes[id]
+        //Check if it has a parent
         if(node.parentId){
-            //Assign the relations
+            //Retrieve the parent from the map and set the relation
             nodes[`${node.parentId}`].setNode(node)
         }
     })
